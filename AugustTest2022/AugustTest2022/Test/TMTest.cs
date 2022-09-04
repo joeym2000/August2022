ed64dfd1ad9f5e6ebda3f41bc9854f1133c83b88
katrinapatio <joeymagpantay3@gmail.com>
2022-09-05 09:49:38 +1200

    Adding wait function

---
+++ AugustTest2022/AugustTest2022/Test/TMTest.cs
@@ -0,0 +1,25 @@
+﻿
+using AugustTest2022.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+// open chrome browser
+IWebDriver driver = new ChromeDriver();
+
+// Login page object initialization and definition
+LogInPage LoginPageObj = new LogInPage();
+LoginPageObj.logInSteps(driver);
+
+//Home page object initialization and definition
+HomePage HomePageObj = new HomePage();
+HomePageObj.GoToHomePage(driver);
+
+//Time And Material 
+TMPage TMPageObj = new TMPage();
+TMPageObj.CreateTM(driver);
+
+//Edit TM
+TMPageObj.EditTM(driver);
+
+//Delete TM
+TMPageObj.DeleteTM(driver);
